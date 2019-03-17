b3d5695431e96114962826cfcf6f6891c636d7af
bwielk <bwielk@gmail.com>
2019-03-17 16:19:28 +0000

    Separated step implementation methods from the generic methods file

--- integration_tests/steps/search_page_functionality.py
+++ integration_tests/steps/search_page_functionality.py
@@ -1,33 +1,33 @@
 from behave import step
-from lib import common_methods as common
+from lib import step_implementation as impl
 
 
 @step('results are filtered by first registration year - {year}')
 def filter_results_by_year(context, year):
-    common.filter_results_by_first_reg_year(context, year)
+    impl.filter_results_by_first_reg_year(context, year)
     css_locator_of_selected_sort_banner = '#app > div > main > div.root___3C6lR.container > div > div.col-md-9 > div >'\
                                           ' div.root___10o2f > ul > li:nth-child(1) > button'
-    selected_sort_banner = common.get_element_by_css_selector(context, css_locator_of_selected_sort_banner)
+    selected_sort_banner = impl.get_element_by_css_selector(context, css_locator_of_selected_sort_banner)
     assert selected_sort_banner is not None and selected_sort_banner.text == 'Erstzulassung von: 2015'
 
 
 @step('the results are sorted by {category} in {order} order')
 def sort_results(context, category, order):
-    common.sort_displayed_results(context, category, order)
+    impl.sort_displayed_results(context, category, order)
 
 
 @step('the results are filtered by first registration - {year}')
 def verify_results_are_filtered_by_registration_year(context, year):
-    results = common.extract_first_reg_data_from_displayed_search_results(context)
+    results = impl.extract_first_reg_data_from_displayed_search_results(context)
     assert all(x >= int(year) for x in results)
 
 
 @step('the results are sorted in {order} order by {category}')
 def verify_results_are_sorted_by_category_in_specific_order(context, order, category):
-    results = common.extract_price_from_displayed_search_results(context)
-    if order == 'descending':
-        sorted_results = sorted(results, reverse=True)
-    else:
-        sorted_results = sorted(results)
-    assert sorted_results == results
-
+    results = impl.extract_price_from_displayed_search_results(context)
+    if category == 'price':
+        if order == 'descending':
+            sorted_results = sorted(results, reverse=True)
+        else:
+            sorted_results = sorted(results)
+        assert sorted_results == results
---
+++ lib/step_implementation.py
@@ -0,0 +1,50 @@
+from selenium.webdriver.support.select import Select
+import time
+from lib.common_methods import get_element_by_css_selector, get_element_by_name
+
+
+def filter_results_by_first_reg_year(context, year):
+    css_locator_of_reg_year_section = '#app > div > main > div.root___3C6lR.container > div > div.col-md-3 > div >' \
+                                      ' div > div > div:nth-child(3) > div.label___3agdr'
+
+    reg_year_section = get_element_by_css_selector(context, css_locator_of_reg_year_section)
+    reg_year_section.click()
+    reg_year_dropdown = get_element_by_name(context, 'yearRange.min')
+    select_reg_date = Select(reg_year_dropdown)
+    select_reg_date.select_by_visible_text(year)
+
+
+def sort_displayed_results(context, category, order):
+    sort_dropdown = get_element_by_name(context, 'sort')
+    select_sort_type = Select(sort_dropdown)
+    order_to_sort_by = None
+    if category == 'price':
+        if order == 'ascending':
+            order_to_sort_by = 'Niedrigster Preis'
+        else:
+            order_to_sort_by = 'Höchster Preis'
+    select_sort_type.select_by_visible_text(order_to_sort_by)
+
+
+def extract_first_reg_data_from_displayed_search_results(context):
+    time.sleep(5)
+    results = []
+    num_of_elements_displayed = get_element_by_name(context, 'pageSize')
+    select_page_size = Select(num_of_elements_displayed)
+    num_of_elements = int(select_page_size.first_selected_option.text)
+    for i in range(1, num_of_elements+1):
+        xpath_to_search_for = '//*[@id="app"]/div/main/div[4]/div/div[2]/div/div[3]/div/div[1]/div[' + str(i) + \
+                              ']/div/a/ul/li[1]'
+        found_element = context.driver.find_element_by_xpath(xpath_to_search_for)
+        results.append(found_element.text)
+    results_years_only_as_ints = [int(x[5:len(x)]) for x in results]
+    return results_years_only_as_ints
+
+
+def extract_price_from_displayed_search_results(context):
+    results = []
+    found_elements = context.driver.find_elements_by_class_name('totalPrice___3yfNv')
+    for element in found_elements:
+        results.append(element.text)
+    results_prices_as_floats = [float(x[0:7]) for x in results]
+    return results_prices_as_floats
