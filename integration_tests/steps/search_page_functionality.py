a9475199278254bea8177c6a9304942e62085d7e
bwielk <bwielk@gmail.com>
2019-03-17 15:44:50 +0000

    Filters are tested and everything works as expected

--- integration_tests/steps/search_page_functionality.py
+++ integration_tests/steps/search_page_functionality.py
@@ -5,7 +5,7 @@ from lib import common_methods as common
 @step('results are filtered by first registration year - {year}')
 def filter_results_by_year(context, year):
     common.filter_results_by_first_reg_year(context, year)
-    css_locator_of_selected_sort_banner = '#app > div > main > div.root___3C6lR.container > div > div.col-md-9 > div >' \
+    css_locator_of_selected_sort_banner = '#app > div > main > div.root___3C6lR.container > div > div.col-md-9 > div >'\
                                           ' div.root___10o2f > ul > li:nth-child(1) > button'
     selected_sort_banner = common.get_element_by_css_selector(context, css_locator_of_selected_sort_banner)
     assert selected_sort_banner is not None and selected_sort_banner.text == 'Erstzulassung von: 2015'
@@ -18,7 +18,8 @@ def sort_results(context, category, order):
 
 @step('the results are filtered by first registration - {year}')
 def verify_results_are_filtered_by_registration_year(context, year):
-    pass
+    results = common.extract_first_reg_data_from_displayed_search_results(context)
+    assert all(x >= int(year) for x in results)
 
 
 @step('the results are sorted in {order} order by {category}')
--- lib/common_methods.py
+++ lib/common_methods.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import *
 from selenium.webdriver.support.select import Select
 import logging
-
+import time
 
 def get_element_by_css_selector(context, selector, timeout=5):
     found_element = None
@@ -39,3 +39,34 @@ def filter_results_by_first_reg_year(context, year):
 
 def sort_displayed_results(context, category, order):
     sort_dropdown = get_element_by_name(context, 'sort')
+    select_sort_type = Select(sort_dropdown)
+    order_to_sort_by = None
+    if category == 'price':
+        if order == 'ascending':
+            order_to_sort_by = 'Niedrigster Preis'
+        else:
+            order_to_sort_by = 'Höchster Preis'
+    select_sort_type.select_by_visible_text(order_to_sort_by)
+
+
+def get_multiple_elements_by_xpath(context, selector):
+    found_elements = context.driver.find_elements_by_xpath(selector)
+    return found_elements
+
+
+def extract_first_reg_data_from_displayed_search_results(context):
+    time.sleep(5)
+    results = []
+    num_of_elements_displayed = get_element_by_name(context, 'pageSize')
+    select_page_size = Select(num_of_elements_displayed)
+    num_of_elements = int(select_page_size.first_selected_option.text)
+    for i in range(1, num_of_elements+1):
+        xpath_to_search_for = '//*[@id="app"]/div/main/div[4]/div/div[2]/div/div[3]/div/div[1]/div[' + str(i) + \
+                              ']/div/a/ul/li[1]'
+        found_element = context.driver.find_element_by_xpath(xpath_to_search_for)
+        results.append(found_element.text)
+    results_years_only_as_ints = [int(x[5:len(x)]) for x in results]
+    return results_years_only_as_ints
+
+def extract_price_from_displayed_search_results(context):
+    found_elements = get_multiple_elements_by_xpath('item___T1IPF')
